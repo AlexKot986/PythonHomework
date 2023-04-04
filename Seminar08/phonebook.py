ae58b7c3077dccd9bb1019e64e908738b0e86eab
Alexershov86 <alexershov86@mail.ru>
2023-04-04 23:12:48 +0300

    дз8 PhoneBook v.02

--- Seminar08/phonebook.py
+++ Seminar08/phonebook.py
@@ -19,7 +19,6 @@ def add_item(filename, lastname = '', firstname = '', secondname = '', phone = '
         if max_id < int(data_array[i][0]): 
             max_id = int(data_array[i][0])
     next_id = max_id + 1
-    # print(next_id)
     lastname = input('Фамилия: ')
     firstname = input('Имя: ')
     secondname = input('Отчество: ')
@@ -31,50 +30,58 @@ def add_item(filename, lastname = '', firstname = '', secondname = '', phone = '
     new_item.append(secondname)
     new_item.append(phone)
     print('Добавлен ID: {}  Фамилия: {}  Имя: {}  Отчество: {}  Телефон: {}'.format(new_item[0], new_item[1], new_item[2], new_item[3], new_item[4]))
-    # print(data_array)
     data_array.append(new_item)
-    # print(data_array)
     write_file(filename, data_array)
 
 def show_all_items(filename):
     data_array = read_file(filename)    
     for i in range(1,len(data_array)):
-        # print("ID:", data_array[i][0], "Фамилия:", data_array[i][1],"Имя:", data_array[i][2], "Отчество:", data_array[i][3], "Телефон:", data_array[i][4])
         print('ID: {}  Фамилия: {}  Имя: {}  Отчество: {}  Телефон: {}'.format(data_array[i][0], data_array[i][1], data_array[i][2], data_array[i][3], data_array[i][4]))
 
 def change_item(filename):
     data_array = read_file(filename)
-    change_ID = input('Введите ID: ')
-    for i in range(1,len(data_array)):
-        if change_ID == data_array[i][0]:
-            lastname = input('Фамилия: ')
-            firstname = input('Имя: ')
-            secondname = input('Отчество: ')
-            phone = input('Телефон: ')
-            data_array[i][1] = lastname
-            data_array[i][2] = firstname
-            data_array[i][3] = secondname
-            data_array[i][4] = phone
-            print('Изменен ID: {}  Фамилия: {}  Имя: {}  Отчество: {}  Телефон: {}'.format(data_array[i][0], data_array[i][1], data_array[i][2], data_array[i][3], data_array[i][4]))
-            # print(data_array)
-            # data_array.append(new_item)
-            # print(data_array)
-            write_file(filename, data_array)
-            return data_array
+    change_ID = find_item(filename)
+    change_user = input('Изменить? Y/N: ')
+    
+    if change_user.upper() == 'Y':
+        lastname = input('Фамилия: ')
+        firstname = input('Имя: ')
+        secondname = input('Отчество: ')
+        phone = input('Телефон: ')
+        data_array[change_ID][1] = lastname
+        data_array[change_ID][2] = firstname
+        data_array[change_ID][3] = secondname
+        data_array[change_ID][4] = phone
+        print('Изменен ID: {}  Фамилия: {}  Имя: {}  Отчество: {}  Телефон: {}'.format(data_array[change_ID][0], data_array[change_ID][1], data_array[change_ID][2], data_array[change_ID][3], data_array[change_ID][4]))
         
-    print('Такого ID нет!')
+        write_file(filename, data_array)
+        return data_array
+    else:               
+        print('Изменение отменено!')
     
 
 def delete_item(filename):
     data_array = read_file(filename)
-    delete_ID = input('Введите ID: ')
+    delete_ID = find_item(filename)
+    delete_user = input('Удалить? Y/N: ')
+
+    if delete_user.upper() == 'Y':             
+        print('Удален ID: {}  Фамилия: {}  Имя: {}  Отчество: {}  Телефон: {}'.format(data_array[delete_ID][0], data_array[delete_ID][1], data_array[delete_ID][2], data_array[delete_ID][3], data_array[delete_ID][4]))
+        data_array.pop(delete_ID)
+        write_file(filename, data_array)
+        return data_array
+    else:
+        print('Удаление отменено!')
+
+
+def find_item(filename):
+    data_array = read_file(filename)
+    find_user = input('Найти: ')
     for i in range(1,len(data_array)):
-        if delete_ID == data_array[i][0]:           
-            print('Удален ID: {}  Фамилия: {}  Имя: {}  Отчество: {}  Телефон: {}'.format(data_array[i][0], data_array[i][1], data_array[i][2], data_array[i][3], data_array[i][4]))
-            data_array.pop(i)
-            write_file(filename, data_array)
-            return data_array
-    print('Такого ID нет!')
+        if find_user in data_array[i]:
+            print('Найден ID: {}  Фамилия: {}  Имя: {}  Отчество: {}  Телефон: {}'.format(data_array[i][0], data_array[i][1], data_array[i][2], data_array[i][3], data_array[i][4]))
+            return i
+    print('Такого нет!')
 
 
 
@@ -94,6 +101,8 @@ def menu():
         change_item(filename)
     elif user_operation == 4:
         delete_item(filename)
+    # elif user_operation == 5:
+    #     find_item(filename)
 
 filename = 'file.txt'
 menu()
